aef182f0b7e3657351eea56fadcffbb4b9acbac2
Carlos <carlosaac232001@gmail.com>
2026-02-19 22:11:33 -0500

    chore: change port to prevent crash

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ import expres from 'express';
 import { indexRouter } from './routes/index.js';
 
 const app = expres();
-const PORT = 8080;
+const PORT = 8000;
 
 app.use('/', indexRouter);
 
-app.listen(PORT, (error) => {
+app.listen(PORT, error => {
   if (error) throw new Error(error);
   console.log(`App working in http://localhost:${PORT}`);
 });
