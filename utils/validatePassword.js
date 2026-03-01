fd7952883947233d5357aede6ec0f2eb1e3be960
Carlos <carlosaac232001@gmail.com>
2026-03-01 00:14:43 -0500

    feat: improve hashed passwords generator function

--- utils/validatePassword.js
+++ utils/validatePassword.js
@@ -1,7 +1,7 @@
 import bcryp from 'bcrypt';
 
-export async function generateHashPassword(password) {
-  return await bcryp.hash(password, 12);
+export async function generateHashPassword(password, salt) {
+  return await bcryp.hash(password, salt);
 }
 
 export async function validateHashedPassword(password, hashedPassword) {
