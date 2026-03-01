2641f10a39b0dae9de9a851ac50401cf3e0d5c71
Carlos <carlosaac232001@gmail.com>
2026-03-01 17:23:52 -0500

    feat: - add PgSessionStore to store session data in PostgreSQL - update to use PgSessionStore for session management - use passport for authentication and session management - add custom host port for docker container

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 import expres from 'express';
-import { indexRouter } from './routes/index.js';
+import { userRoutes } from './routes/userRoutes.js';
+import { messageRoutes } from './routes/messageRoutes.js';
+import './config/passport.js';
+import session from 'express-session';
+import { pool } from './db/pool.js';
+import connectPgSimple from 'connect-pg-simple';
+import passport from 'passport';
 
 const app = expres();
 const PORT = 8000;
 
-app.use('/', indexRouter);
+const PgSession = connectPgSimple(session);
+const sessionStore = new PgSession({
+  pool,
+  tableName: 'session',
+});
 
-app.listen(PORT, error => {
+app.use(expres.json());
+app.use(expres.urlencoded({ extended: true }));
+
+const ONE_DAY = 1000 * 60 * 60 * 24;
+
+app.use(
+  session({
+    store: sessionStore,
+    secret: process.env.SECRET_WORD,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: ONE_DAY },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/users', userRoutes);
+app.use('/messages', messageRoutes);
+
+app.listen(PORT, '0.0.0.0', error => {
   if (error) throw new Error(error);
   console.log(`App working in http://localhost:${PORT}`);
 });
