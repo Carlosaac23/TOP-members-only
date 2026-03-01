0b822f9e53d4831cb40594ecdf8664e0a6071367
Carlos <carlosaac232001@gmail.com>
2026-03-01 17:26:13 -0500

    feat: create user services functions

---
+++ services/userService.js
@@ -0,0 +1,29 @@
+import { insertUser, getUserById } from '../db/userQueries.js';
+import { generateHashPassword } from '../utils/validatePassword.js';
+
+export async function createUser(data) {
+  console.log('Data in service:', data);
+  const hashedPassword = await generateHashPassword(data.password_hash, 12);
+
+  return await insertUser({
+    ...data,
+    password_hash: hashedPassword,
+  });
+}
+
+export async function getUser(userId) {
+  const user = await getUserById(userId);
+  const userPassword = user.password_hash;
+  bcrypt.compare('estaeslamejorpassword', userPassword, (error, result) => {
+    if (error) {
+      console.error('Error comparing passwords');
+    }
+
+    if (result) {
+      console.log('Passwords match! User authenticated.');
+    } else {
+      console.log('Passwords do not match! Authentication failed.');
+    }
+  });
+  return user;
+}
