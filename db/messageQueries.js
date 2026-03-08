5b483b2dfee76bd8e3ce937711a10aa577e7184c
Carlos <carlosaac232001@gmail.com>
2026-03-08 16:48:59 -0500

    refactor: reorganize message query functions for clarity and maintainability

--- db/messageQueries.js
+++ db/messageQueries.js
@@ -1,12 +1,5 @@
 import { pool } from './pool.js';
 
-export async function getMessages() {
-  const { rows } = await pool.query(
-    'SELECT messages.*, users.username FROM messages INNER JOIN users ON messages.user_id = users.id'
-  );
-  return rows;
-}
-
 export async function createMessage(data) {
   console.log('Message from controller:', data);
   return await pool.query('INSERT INTO messages (title, content, user_id) VALUES ($1, $2, $3)', [
@@ -16,7 +9,23 @@ export async function createMessage(data) {
   ]);
 }
 
-export async function getMessage(messageId) {
-  const { rows } = await pool.query('SELECT * FROM messages WHERE id = $1', [messageId]);
-  return rows[0];
+export async function getMessages() {
+  const { rows } = await pool.query(
+    'SELECT messages.*, users.username FROM messages INNER JOIN users ON messages.user_id = users.id'
+  );
+  return rows;
 }
+
+export async function getMessagesFromUser(userId) {
+  const { rows } = await pool.query('SELECT * from messages WHERE user_id = $1', [userId]);
+  return rows;
+}
+
+export async function deleteMessageById(messageId) {
+  await pool.query('DELETE FROM messages WHERE id = $1', [messageId]);
+}
+
+// export async function getMessage(messageId) {
+//   const { rows } = await pool.query('SELECT * FROM messages WHERE id = $1', [messageId]);
+//   return rows[0];
+// }
